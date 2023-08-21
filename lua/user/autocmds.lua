a1c0d9dd2186b316b4d32c99f6a42c3e75936733
BrunoBMoura <brunomoura.meta@gmail.com>
2023-08-21 11:52:10 -0300

    feat: add formatting for c files.

--- lua/user/autocmds.lua
+++ lua/user/autocmds.lua
@@ -2,13 +2,14 @@ local augroup = vim.api.nvim_create_augroup
 local autocmd = vim.api.nvim_create_autocmd
 local user_group = augroup("user", {})
 
-autocmd(
-  "BufWritePre",
-  {group = user_group, pattern = "*", command = "%s/\\s\\+$//e"}
-)
+autocmd("BufWritePre", {
+  group = user_group,
+  pattern = "*",
+  command = "%s/\\s\\+$//e"
+})
 
 local lang_meta_op = {
-  c      = { format = "clang-format"},
+  c      = { format = "clang-format -i"},
   rust   = { format = "rustfmt", build = "cargo build", exec = "cargo run"},
   ruby   = { small_indent = true, exec = "ruby" },
   lua    = { small_indent = true, exec = "lua" },
@@ -17,21 +18,21 @@ local lang_meta_op = {
 
 for lang, data in pairs(lang_meta_op) do
   if data.format ~= nil then
-    autocmd(
-      "FileType",
-      {command = "nnoremap <leader><leader>f :!" .. data.format .. " % <CR>", pattern = lang}
-    )
+    autocmd( "FileType", {
+      command = "nnoremap <leader><leader>f :!" .. data.format .. " % <CR>",
+      pattern = lang
+    })
   end
   if data.small_indent ~= nil then
-    autocmd(
-      "FileType",
-      {command = "setlocal shiftwidth=2 tabstop=2", pattern = lang}
-    )
+    autocmd("FileType", {
+      command = "setlocal shiftwidth=2 tabstop=2",
+      pattern = lang
+    })
   end
   if data.exec then
-    autocmd(
-      "FileType",
-      {command = "nnoremap <leader><leader>e :vsplit <CR>:ter " .. data.exec .. " % <CR>", pattern = lang}
-    )
+    autocmd("FileType", {
+      command = "nnoremap <leader><leader>e :vsplit <CR>:ter " .. data.exec .. " % <CR>",
+      pattern = lang
+    })
   end
 end
